21ac41f6e2632e4a507632a5d9f94023497c7a38
kavin1231 <chamathkaveendra@gmail.com>
2025-01-17 11:23:50 +0530

    create approve review router™️

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addReview,
+  approveReview,
   deleteReviews,
   getReviews,
 } from "../controllers/reviewController.js";
@@ -10,12 +11,15 @@ const reviewRouter = express.Router();
 reviewRouter.post("/", addReview);
 reviewRouter.get("/", getReviews);
 reviewRouter.delete("/:email", deleteReviews);
+reviewRouter.put("/approved/:email", approveReview);
 
-reviewRouter.get("/approved", (req, res) => {
+/*reviewRouter.get("/approved", (req, res) => {
   console.log("This is approved route");
 });
 reviewRouter.get("/email", (req, res) => {
   console.log("This is email route");
-});
+});*/
+
+
 
 export default reviewRouter;
