61fd920cc599c213ebb487fab656c6c55c128bc7
kavin1231 <chamathkaveendra@gmail.com>
2025-01-17 10:25:31 +0530

    create approve review function™️

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -88,3 +88,39 @@ export function deleteReviews(req, res) {
     }
   }
 }
+
+export function approveReview(req, res) {
+  const email = req.params.email;
+
+  if (req.user == null) {
+    res.status(401).json({
+      message: "Please login and try again",
+    });
+    return;
+  }
+
+  if (req.user.role == "admin") {
+    Review.updateOne(
+      {
+        email: email,
+      },
+      {
+        isApproved: true,
+      }
+    )
+      .then(() => {
+        res.json({
+          message: "Review approved successfully",
+        });
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "Review approval failed",
+        });
+      });
+  } else {
+    res.status(403).json({
+      message: "You are not an admin.Only admins can approve the reviews.",
+    });
+  }
+}
