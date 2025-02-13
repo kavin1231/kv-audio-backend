054da362fdb67b66b276f28df6354649d12771df
kavin1231 <chamathkaveendra@gmail.com>
2025-02-13 15:53:51 +0530

    correctly add get review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -28,15 +28,20 @@ export function addReview(req, res) {
     });
 }
 
-export async function getReviews(req, res) {
+export function getReviews(req, res) {
   const user = req.user;
 
-  
-  
+  if (user == null || user.role != "admin") {
+    Review.find({ isApproved: true }).then((reviews) => {
+      res.json(reviews);
+    });
+    return;
+  }
+  if (user.role == "admin") {
     Review.find().then((reviews) => {
       res.json(reviews);
     });
-  
+  }
 }
 
 export function deleteReviews(req, res) {
