bade479bd724b4a02dfe14998784194868478ddc
kavin1231 <chamathkaveendra@gmail.com>
2025-02-14 15:12:35 +0530

    create get products function for customer can see😊

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,11 +19,21 @@ export async function addProduct(req, res) {
   try {
     await newProduct.save();
     res.json({
-      message: "Product added successfully",
+      message: "Product addedd successfully",
     });
   } catch (error) {
     res.status(500).json({
       error: "Product addition failed",
+    });
+  }
+}
+export async function getProducts(req, res) {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to get products",
     });
   }
 }
