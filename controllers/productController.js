c8c9510246f149fdf5b2ae1d44d8394e46886d5e
kavin1231 <chamathkaveendra@gmail.com>
2025-02-18 19:28:32 +0530

    successfully created update feature😊

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,13 +4,13 @@ import { isItAdmin } from "./userController.js";
 export async function addProduct(req, res) {
   if (req.user == null) {
     req.status(401).json({
-      message: "Please login and try again"
+      message: "Please login and try again",
     });
     return;
   }
   if (req.user.role != "admin") {
     res.status(403).json({
-      message: "you are not authorized to perform this action"
+      message: "you are not authorized to perform this action",
     });
     return;
   }
@@ -35,13 +35,39 @@ export async function getProducts(req, res) {
       res.json(products);
       return;
     } else {
-      const products = await Product.find({availability:true});
+      const products = await Product.find({ availability: true });
       res.json(products);
       return;
     }
   } catch (e) {
     res.status(500).json({
       message: "Failed to get products",
+    });
+  }
+}
+export async function updateProduct(req, res) {
+  try {
+    if (isItAdmin(req)) {
+
+      const key=req.params.key;
+
+      const data=req.body;
+
+      await Product.updateOne({key:key},data)
+
+      res.json({
+        message:"Product updated successfully"
+      })
+      return;
+
+    } else {
+      res.status(403).json({
+        messege: "You are not authorized person to perform this action",
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Failed to update product",
     });
   }
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addProduct, getProducts } from "../controllers/productController.js";
+import {
+  addProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", addProduct);
 
-productRouter.get("/",getProducts);
+productRouter.get("/", getProducts);
+
+productRouter.put("/:key", updateProduct);
 
 export default productRouter;
