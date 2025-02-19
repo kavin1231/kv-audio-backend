5fab8279be062d7daa007fa3034970d1c83ec021
kavin1231 <chamathkaveendra@gmail.com>
2025-02-19 18:58:39 +0530

    successfully addedd inquiry all

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,23 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
+dotenv.config();
 export function registerUser(req, res) {
   const data = req.body;
 
   data.password = bcrypt.hashSync(data.password, 10);
-
+  //#
   const newUser = new User(data);
 
   newUser
     .save()
     .then(() => {
-      res.json({
-        message: "User registered Successfully",
-      });
+      res.json({ message: "User registered successfully" });
     })
     .catch((error) => {
-      res.status(500).json({
-        message: "user registration failed",
-      });
+      res.status(500).json({ error: "User registration failed" });
     });
 }
 
@@ -37,11 +34,12 @@ export function loginUser(req, res) {
         data.password,
         user.password
       );
+
       if (isPasswordCorrect) {
-        const token = jsonwebtoken.sign(
+        const token = jwt.sign(
           {
-            firstname: user.firstname,
-            lastname: user.lastname,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
             role: user.role,
             profilePicture: user.profilePicture,
@@ -49,32 +47,36 @@ export function loginUser(req, res) {
           },
           process.env.JWT_SECRET
         );
-        res.json({
-          message: "Login successfull",
-          token: token,
-        });
+
+        res.json({ message: "Login successful", token: token , user: user});
       } else {
-        res.status(401).json({
-          error: "Login unsuccessfull",
-        });
+        res.status(401).json({ error: "Login failed" });
       }
     }
-  });
+  }); 
 }
-export function isItAdmin(req) {
+
+
+export function isItAdmin(req){
   let isAdmin = false;
-  if (req.user != null) {
-    if (req.user.role == "admin") {
+
+  if(req.user != null){
+    if(req.user.role == "admin"){
       isAdmin = true;
     }
   }
+
   return isAdmin;
 }
-export function isItCustomer(req) {
+
+export function isItCustomer(req){
   let isCustomer = false;
-  if (req.user != null) {
-    if (req.user.role == "customer") {
+
+  if(req.user != null){
+    if(req.user.role == "customer"){
       isCustomer = true;
     }
   }
+
+  return isCustomer;
 }
